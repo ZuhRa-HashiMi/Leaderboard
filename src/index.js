f07d5a3683547d6258458edb8dcb5772b2250972
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-11-24 16:03:45 +0430

    solve the lintes errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
+import * as util from './module/app/app.js';
+
 const name = document.getElementById('name');
 const score = document.getElementById('score');
 const submit = document.getElementById('submit');
 
 submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    util.addToLeaderboard({ user: name.value, score: score.value });
-    score.value = '';
-    name.value = '';
-  });
-  
+  e.preventDefault();
+  util.addToLeaderboard({ user: name.value, score: score.value });
+  score.value = '';
+  name.value = '';
+});
 
-  window.addEventListener('load', () => {
-    util.displayLeaderboard();
-  });
-  
+window.addEventListener('load', () => {
+  util.displayLeaderboard();
+});
