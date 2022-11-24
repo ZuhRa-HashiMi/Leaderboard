385f6af29e0fed763ce09503c8dd6de726d45319
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-11-24 23:37:10 +0430

    add async to setData fanction

--- src/module/game/game.js
+++ src/module/game/game.js
@@ -6,6 +6,14 @@ const getData = async () => {
   return scoreList;
 };
 
-
+const setData = async (userName, userScore) => {
+  const connect = await fetch(baseURL, {
+    method: 'POST',
+    body: JSON.stringify({ user: userName, score: userScore }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  });
+  const receivedData = await connect.json();
+  return receivedData;
+};
 
 export { getData, setData };
